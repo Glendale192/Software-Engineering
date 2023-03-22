6028d5749399d551eddec8477ca2d2405493fcaf
Vlad Furs <glendale@stu.cn.ua>
2023-03-22 11:00:24 +0200

    Новий файл розробника

---
+++ Developer.cs
@@ -0,0 +1,28 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_Email_Client
+{
+    public partial class Developer : DevExpress.XtraEditors.XtraForm
+    {
+        public Developer()
+        {
+            InitializeComponent();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
+
+// Програму розробив студент групи КІт-212 Фурс В.С.
